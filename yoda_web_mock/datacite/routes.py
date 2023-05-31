e7ab8afa14a28948238f0f2e233bebabbadc9372
Sietse Snel <s.t.snel@uu.nl>
2023-05-31 16:03:41 +0200

    Datacite mock: fix metadata update endpoints

--- yoda_web_mock/datacite/routes.py
+++ yoda_web_mock/datacite/routes.py
@@ -13,25 +13,19 @@ def index():
     return Response("Yoda mock: Datacite")
 
 
-@blueprint_datacite.route('/doi/<doi>', methods=['GET'])
+@blueprint_datacite.route('/doi/<path:doi>', methods=['GET'])
 def check_doi_availability(doi):
     return Response("Check DOI availability response (mocked)",
                     status=404)  # 404 means the DOI is available
 
 
-@blueprint_datacite.route('/metadata/<doi>', methods=['PUT'])
-def register_doi_metadata(doi):
-    return Response("Register DOI metadata response (mocked)",
-                    status=201)  # 201 means metadata has been updated
+@blueprint_datacite.route('/dois/<path:doi>', methods=['PUT'])
+def update_doi_url(doi):
+    # 200 means successful update
+    return Response("Update DOI response (mocked)", status=200)
 
 
-@blueprint_datacite.route('/metadata/<doi>', methods=['DELETE'])
-def delete_doi_metadata(doi):
-    # 200 means metadata has been removed
-    return Response("Delete metadata (mocked)", status=200)
-
-
-@blueprint_datacite.route('/doi/<doi>', methods=['PUT'])
-def register_doi_url(doi):
+@blueprint_datacite.route('/dois', methods=['POST'])
+def register_doi_url():
     # 201 means URL has been updated
     return Response("Register DOI response (mocked)", status=201)
