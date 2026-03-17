816faeff1f6adc49eb2e830eebf4d4517105304f
Lazlo Westerhof <l.r.westerhof@uu.nl>
2026-03-17 10:07:13 +0100

    YDA-6398: add json storage to SRAM mock

--- yoda_web_mock/app.py
+++ yoda_web_mock/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__copyright__ = 'Copyright (c) 2023, Utrecht University'
+__copyright__ = 'Copyright (c) 2023-2026, Utrecht University'
 __license__ = 'GPLv3, see LICENSE'
 
 import logging
